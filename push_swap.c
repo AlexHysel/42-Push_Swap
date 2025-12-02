05bd905c4e3505d5a9b20889b7107cc4f12fc787
Alexandr Fomin <afomin@u80z06s02.42kl.edu.my>
2025-12-02 13:34:30 +0800

    get_diff fixed and renamed to get_common

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: afomin afomin@student.42kl.edu.my          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:11:14 by afomin            #+#    #+#             */
-/*   Updated: 2025/12/02 12:56:58 by afomin           ###   ########.fr       */
+/*   Updated: 2025/12/02 13:24:40 by afomin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,45 +51,48 @@ static void	final_rotate(t_stack *a)
 	stack_rotate(distance_to_top(min_index, a->size), a);
 }
 
-static short	get_diff(short *a_cost, short *b_cost)
+static short	get_common(short *a_cost, short *b_cost)
 {
-	short	diff;
+	short	common;
 
-	diff = 0;
+	common = 0;
 	if ((*a_cost > 0 && *b_cost > 0) || (*a_cost < 0 && *b_cost < 0))
 	{
 		if (*a_cost > *b_cost)
 		{
 			*a_cost -= *b_cost;
-			diff = *a_cost;
+			common = *b_cost;
 			*b_cost = 0;
 		}
 		else
 		{
 			*b_cost -= *a_cost;
-			diff = *b_cost;
+			common = *a_cost;
 			*a_cost = 0;
 		}
 	}
-	return (diff);
+	return (common);
 }
 
 void	push_swap(t_stack *a, t_stack *b)
 {
 	short	a_cost;
 	short	b_cost;
-	short	diff;
+	short	common;
 
-	stack_push(a->size - 3, a, b);
-	sort_3(a);
-	while (b->size)
+	if (a->size > 2)
 	{
-		get_cheapest(a, b, &a_cost, &b_cost);
-		diff = get_diff(&a_cost, &b_cost);
-		stacks_rotate(diff, a, b);
-		stack_rotate(a_cost, a);
-		stack_rotate(b_cost, b);
-		stack_push(1, b, a);
+		stack_push(a->size - 3, a, b);
+		sort_3(a);
+		while (b->size)
+		{
+			get_cheapest(a, b, &a_cost, &b_cost);
+			common = get_common(&a_cost, &b_cost);
+			stacks_rotate(common, a, b);
+			stack_rotate(a_cost, a);
+			stack_rotate(b_cost, b);
+			stack_push(1, b, a);
+		}
 	}
 	final_rotate(a);
 }
